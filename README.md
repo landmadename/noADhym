e68b2d4c6911e52630d67808029a078e950d4377
landmadename <landmadename@example.com>
2021-07-06 13:45:27 +0800

    V1

---
+++ README.md
@@ -0,0 +1,3 @@
+# 淘宝京东超便宜 · telegram消息筛选转发机器人
+通过config.py来配置筛选关键词
+通过config.ini来设置代理
---
+++ main.py
@@ -0,0 +1,56 @@
+from pyrogram import Client, filters
+import config
+import re
+from datetime import datetime
+from collections import deque
+from fuzzywuzzy import fuzz
+
+app = Client("******")
+chat_id = "noADhym"
+pool = deque(maxlen=10)
+
+def keywords_check(client, message):
+    data = ("" if message.text is None else message.text) + ("" if message.caption is None else message.caption)
+    for keyword in config.keywords:
+        if re.search(keyword, data) is not None:
+            for i in pool:
+                if fuzz.ratio(data, i)>60:
+                    return False
+            pool.append(data)
+            client.send_message(chat_id, "关键词："+keyword)
+            return True
+    return False
+
+def channel_check(client, message):
+    if message.sender_chat is None:
+        return False
+    channel_name = message.sender_chat["username"]
+    if channel_name not in config.channel_rules:
+        return False
+    rules = config.channel_rules[channel_name]
+    data = ("" if message.text is None else message.text) + ("" if message.caption is None else message.caption)
+    for rule in rules:
+        if re.search(rule, data) is not None:
+            for i in pool:
+                if fuzz.ratio(data, i)>60:
+                    return False
+            pool.append(data)
+            client.send_message(chat_id, "关键词："+rule)
+            return True
+    return False
+
+@app.on_message()
+def my_handler(client, message):
+    print(message.text)
+    print(message.caption)
+    print(datetime.now())
+    print("--------------")
+    if keywords_check(client, message) is True:
+        message.forward(chat_id)
+        return
+    if channel_check(client, message) is True:
+        message.forward(chat_id)
+        return
+
+
+app.run()
